22ad773d7f763a56f9a471ef3a0f4beebac0b782
MuskanParlani90 <76032573+MuskanParlani90@users.noreply.github.com>
2020-12-17 13:15:51 +0530

    Update Program.cs

--- SampleProjectJenkins/Program.cs
+++ SampleProjectJenkins/Program.cs
@@ -11,7 +11,7 @@ namespace SampleProjectJenkins
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Console.WriteLine("Hi")
+            Console.WriteLine("Hi");
         }
     }
 }
